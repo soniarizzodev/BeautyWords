8e9bcc8eda8529d88650e4fdb28b0579f57352e9
soniarizzodev <soniarizzo.dev@gmail.com>
2020-06-23 20:28:26 +0200

    Integrating more fonts

--- webapp/static/scripts/viewmodels.js
+++ webapp/static/scripts/viewmodels.js
@@ -5,7 +5,7 @@ function App() {
     let _self = this;
     this.SplitSentence = ko.observableArray([]);
     this.InputSentence = ko.observable('');
-    this.Fonts = ko.observableArray(['Lobster', 'LobsterTwo', 'PermanentMarker', 'DancingScript', 'SpecialElite', 'Satisfy']);
+    this.Fonts = ko.observableArray(['Lobster', 'LobsterTwo', 'PermanentMarker', 'DancingScript', 'SpecialElite', 'Satisfy', 'FugazOne', 'Spinnaker', 'Cinzel']);
     this.Colors = ko.observableArray(
         ['color1', 'color2', 'color3', 'color4', 'color5', 'color6', 'color7', 'color8', 'color9', 'color10']
     );
